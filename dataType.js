21cb77a290f94405c7a9ad9bf97ed4a0ad12f2c5
githubinfovikas <vikasdbg453@gmail.com>
2024-06-24 09:54:49 +0530

    add

---
+++ dataType.js
@@ -0,0 +1,16 @@
+"use strict"         // treat all js code as strict mode new version
+
+
+
+
+const data  = {
+    name:"vikas Kumar",
+    age:22,
+    address:"Muzaffarpur, Bihar",
+    registration :"12345",
+    college :"MIT Muzaffarpur",
+    active:true
+};
+console.log(data);
+console.table(data);
+console.log(typeof data.name)
---
+++ tableConsole.js
@@ -0,0 +1,9 @@
+let name="vikas Kumar";
+let age=22;
+let address="Muzaffarpur, Bihar";
+let registration="12345";
+let college ="MIT Muzaffarpur";
+
+
+console.table({name,age,address,registration,college});
+
